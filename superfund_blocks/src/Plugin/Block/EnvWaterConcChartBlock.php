ec50b303ae2b39419b0dc682d302de52e90034da
Barton <bartonm@oregonstate.edu>
2026-07-29 14:46:56 -0700

    changing icon

--- superfund_blocks/src/Plugin/Block/EnvWaterConcChartBlock.php
+++ superfund_blocks/src/Plugin/Block/EnvWaterConcChartBlock.php
@@ -242,8 +242,8 @@ class EnvWaterConcChartBlock extends BlockBase implements BlockPluginInterface, 
     type: 'bar',
     x: sampleIds,
     y: values,
-    text: sampleNames,
-    hovertemplate: '%{text}<br>%{y} ' + unit + '<extra></extra>',
+    //text: sampleNames,
+    hovertemplate: '%{sampleNames}<br>%{y} ' + unit + '<extra></extra>',
   };
 
   var layout = {
